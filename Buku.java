a036cacfdb18826a3e9639eff9bbebe2770914bc
syahidah-hash <syahidahazzahraptri@gmail.com>
2025-02-16 00:36:41 +0700

    Create Buku.java

---
+++ Buku.java
@@ -0,0 +1,21 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package masyaallah;
+
+/**
+ *
+ * @author ACER
+ */
+public class Buku {
+    public static void main(String[] args) {
+        System.out.println("Hello ");
+        System.out.println("World !");
+        
+        System.out.print("Hello ");
+        System.out.print("World !");
+
+    }
+}
+
