9797b81db836e9f951e51775112fb874902648e9
syahidah-hash <syahidahazzahraptri@gmail.com>
2025-02-16 00:34:55 +0700

    Create Person.java

---
+++ Person.java
@@ -0,0 +1,20 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package masyaallah;
+public class Person{
+    String Name;
+    int Age;  
+public Person (String Name, int Age){
+    this.Name = Name;
+    this.Age = Age;
+} 
+ public String getName() {
+    return this.Name;
+}
+ public int getAge(){
+    return this.Age;
+}
+}
+ 
